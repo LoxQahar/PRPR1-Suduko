572a078fa10107c7adaf60c2b85347626d9bee85
LoxQahar <qaharhassani@gmail.com>
2025-04-05 17:37:11 +0200

    the board and all the basic rules done

--- PRPR1 Suduko/Program.cs
+++ PRPR1 Suduko/Program.cs
@@ -89,14 +89,93 @@ namespace PRPR1_Suduko
 
         }
 
+        static bool validMoves(int x, int y, int num)
+        {
+            for (int col = 0; col < 9; col++)
+            {
+                if (col != x && board[y, col] == num)
+                    return false;
+            }
+
+            for (int row = 0; row < 9; row++)
+            {
+                if (row != y && board[row, x] == num)
+                    return false;
+            }
+
+            int blockX = (x / 3) *3;
+            int blockY = (y / 3) * 3;
+
+            for (int row = blockY; row < blockY + 3; row++)
+            {
+                for (int col = blockX; col < blockX + 3; col++)
+                {
+                    if ((row != y || col != x) && board[row, col] == num)
+                        return  false;
+                }
+            }
+            return true;
+        }
+
+
+
         static void Main(string[] args)
         {
-            Console.Title = "Suduko Game";
-            Console.CursorVisible = false;
 
             drawBoard2();
             numInStart();
-            
+
+
+            bool running = true;
+            while (running)
+            {
+                var key = Console.ReadKey(true).Key;
+
+                switch (key)
+                {
+
+                    case ConsoleKey.UpArrow:
+                        if (cursorY > 0) cursorY--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (cursorY < 8) cursorY++;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        if (cursorX > 0) cursorX--;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (cursorX < 8) cursorX++;
+                        break;
+
+                    case ConsoleKey.D1:
+                    case ConsoleKey.D2:
+                    case ConsoleKey.D3:
+                    case ConsoleKey.D4:
+                    case ConsoleKey.D5:
+                    case ConsoleKey.D6:
+                    case ConsoleKey.D7:
+                    case ConsoleKey.D8:
+                    case ConsoleKey.D9:
+
+                    int num = (int)key - (int)ConsoleKey.D0;
+                        
+                    if (validMoves(cursorX, cursorY, num))
+                    {
+                   
+                            board[cursorY, cursorX] = num;
+                    }
+                    break;
+
+                    case ConsoleKey.Backspace:
+                        board[cursorY, cursorX] = 0;
+                        break;
+
+                }
+
+                drawBoard2();
+            }
+
+
         }
-    }
+    }   
 }
